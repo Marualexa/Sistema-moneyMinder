058cf3b460f2e4bea92d02170f2afea1faef66e0
Mariana <contrerasomanam@gmail.com>
2023-08-03 11:55:22 -0400

    Subido a git-action

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
   plugins: [vue()],
+  base: '/Sistema-moneyMinder/',
   resolve: {
     alias: {
       '@img': fileURLToPath(new URL('src/assets/', import.meta.url)),
