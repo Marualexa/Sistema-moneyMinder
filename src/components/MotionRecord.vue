4189dc66244505f0f2a5df8a4900b3b51fe9c740
Mariana <contrerasomanam@gmail.com>
2023-08-04 18:46:19 -0400

    Arreglos del formulario de ingreso de movimiento 04/08

--- src/components/MotionRecord.vue
+++ src/components/MotionRecord.vue
@@ -2,17 +2,23 @@
   <div class="container">
     <div class="title-prin">
       <p>Nuevo movimiento</p>
-      <img src="../assets/closet.svg" alt="" />
+      <img @click="closet" src="../assets/closet.svg" alt="" />
     </div>
-    <form @submit.prevent="submit">
+    <form @submit.prevent="createMovent">
       <div class="mb-3">
         <label for="" class="form-label">Titulo</label>
-        <input type="text" v-model="title" class="form-control" />
+        <input type="text" v-model="registMovent.title" class="form-control" />
+        <div v-if="hasError('title')" class="invalid-feedback">
+          {{ errorObject.errorMessage }}
+        </div>
       </div>
 
       <div class="mb-3">
         <label for="" class="form-label">Monto</label>
-        <input type="number" v-model="amount" class="form-control" />
+        <input type="number" v-model="registMovent.amount" class="form-control" />
+        <div v-if="hasError('amount')" class="invalid-feedback">
+          {{ errorObject.errorMessage }}
+        </div>
       </div>
 
       <div class="mb-3">
@@ -20,22 +26,31 @@
         <textarea
           class="form-control"
           rows="3"
-          v-model="description"
+          v-model="registMovent.description"
         ></textarea>
+        <div v-if="hasError('description')" class="invalid-feedback">
+          {{ errorObject.errorMessage }}
+        </div>
       </div>
 
       <div class="field">
         <label class="radio-label">
-          <input type="radio" v-model="movementType" value="Ingreso" />
+          <input type="radio" v-model="registMovent.movementType" value="Ingreso" />
           <span>Ingreso</span>
         </label>
+        <div v-if="hasError('movementType')" class="invalid-feedback">
+          {{ errorObject.errorMessage }}
+        </div>
       </div>
 
       <div class="field">
         <label class="radio-label">
-          <input type="radio" v-movel="movementType" value="Gasto" />
+          <input type="radio" v-model="registMovent.movementType" value="Gasto" />
           <span>Gasto</span>
         </label>
+        <div v-if="hasError('movementType')" class="invalid-feedback">
+          {{ errorObject.errorMessage }}
+        </div>
       </div>
       <div class="action">
         <button @click="removeMovent">Agregar movimiento</button>
@@ -46,11 +61,73 @@
 
 <script setup>
 import { useRouter } from "vue-router";
+import { useAsync } from "../hooks/useAsync";
+import { reactive, computed } from "vue";
+import { Joi } from "vue-joi-validation";
 
 const router = useRouter();
+const { result, makeRequest, errorData } = useAsync();
 
-function removeMovent() {
-  router.push({ name: "Home" });
+const registMovent = reactive({
+  title: "",
+  amount: "",
+  description: "",
+  movementType: "",
+});
+
+const errorObject = reactive({
+  errorName: "",
+  errorMessage: "",
+});
+
+const data = {
+  title: Joi.string().required().max(5),
+  amount: Joi.number().positive().precision(2).required(),
+  description: Joi.string().required(),
+  movementType: Joi.string().valid('Ingreso', 'Gasto').required(),
+};
+
+const hasError = computed(() => {
+  return (property) => {
+    return errorObject.errorName === property;
+  };
+});
+
+function createMovent() {
+  const resultForm = Joi.validate(registMovent, data, async (err, value) => {
+    if (err) {
+      let starForm = err.message;
+      let starIndex = starForm.indexOf("[") + 1;
+      let endIndex = starForm.indexOf("]");
+      let element = starForm.substring(starIndex, endIndex);
+
+      let cadena = element;
+
+      const string = cadena.slice(1);
+      const string2 = string.indexOf("'");
+      const final = string.slice(0, string2);
+      let messageIndix = string.slice(string2 + 1);
+
+      errorObject.errorName = final;
+      errorObject.errorMessage = final + " " + messageIndix;
+    } else {
+      await makeRequest("moments", {}, "POST", {
+        title: registMovent.title,
+        amount: registMovent.amount,
+        description: registMovent.description,
+        movementType: registMovent.movementType,
+      });
+      registMovent.title = "";
+      registMovent.amount = "";
+      registMovent.description = "";
+      registMovent.movementType = "Ingreso";
+      router.push({ name: "ImportApp", params: { id: result.value.id } });
+    }
+  });
+}
+
+function closet() {
+  router.push({ name: "ImportApp" });
 }
 </script>
 
--- src/router/index.js
+++ src/router/index.js
@@ -3,9 +3,10 @@ import { createRouter, createWebHistory } from 'vue-router';
 const router = createRouter({
     history: createWebHistory(),
     routes: [
-        { path: '/moneyminder/', name: 'Home', component: () => import('../components/ImportApp.vue') },
-        { path: '/newMovent', name: 'MotionRecord', component: () => import('../components/MotionRecord.vue') },
-    ]
+        { path: '/moneyminder/', name: 'ImportApp', component: () => import('../components/ImportApp.vue') },
+        { path: '/moneyminder/newMovent', name: 'MotionRecord', component: () => import('../components/MotionRecord.vue') },
+        { path: '/moneyminder/history/:id', name: 'HistoryOfMovent', component: () => import('../components/Movement/HistoryOfMovements.vue') },
+    ],
 });
 
 export default router
